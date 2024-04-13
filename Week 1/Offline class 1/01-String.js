9dc371845ad7874082e93b35a4289786d1c2ba68
Vinal Pawar <vinalpawar1@gmail.com>
2024-04-13 18:59:18 +0530

    commit

---
+++ Week 1/Offline class 1/01-String.js
@@ -0,0 +1,69 @@
+// length
+// function getLength(str) {
+//   console.log("Original String: ", str);
+//   console.log("Length: ", str.length);
+// }
+// getLength("Hello World");
+
+// indexOf
+// function findIndexOf(str, target) {
+//   console.log("Original String: ", str);
+//   console.log("Index: ", str.indexOf(target));
+// }
+// findIndexOf("Hello World World", "World");
+
+// lastIndexOf
+// function findIndexOf(str, target) {
+//   console.log("Original String: ", str);
+//   console.log("Index: ", str.lastIndexOf(target));
+// }
+// findIndexOf("Hello World World", "World");
+
+// slice
+// function getSlice(str, start, end) {
+//   console.log("Original String: ", str);
+//   console.log("After Slice: ", str.slice(start, end));
+// }
+// getSlice("Hello World", 0, 5);
+
+//substr
+// const value = "harkirat singh";
+// let ans = value.substr(2, 5);
+// let ans2 = value.slice(2, 5);
+// console.log(ans);
+// console.log(ans2);
+
+// replace
+// function replaceString(str, target, replacement) {
+//   console.log("Original String: ", str);
+//   console.log("After replace: ", str.replace(target, replacement));
+// }
+// replaceString("Hello World", "World", "Javascript");
+
+// split
+// function splitString(str, separator) {
+//   console.log("Original String: ", str);
+//   console.log("After Split: ", str.split(separator));
+// }
+// splitString("Hello World", " ");
+
+// trim
+// function trimString(str) {
+//   console.log("Original String: ", str);
+//   console.log("After trim: ", str.trim());
+// }
+// trimString(" Hello World ");
+
+// toUpperCase
+// function toUpper(str) {
+//   console.log("Original String: ", str);
+//   console.log("After toUpperCase: ", str.toUpperCase());
+// }
+// toUpper("Hello World");
+
+// toLowerCase
+// function toLower(str) {
+//   console.log("Original String: ", str);
+//   console.log("After toLowerCase: ", str.toLowerCase());
+// }
+// toLower("Hello World");
---
+++ Week 1/Offline class 1/02-Number.js
@@ -0,0 +1,21 @@
+// parseInt
+
+// function explainParseInt(value) {
+//   console.log("Original Value: ", value);
+//   let result = parseInt(value);
+//   console.log("After parseInt: ", result);
+// }
+// explainParseInt("42");
+// explainParseInt("42px");
+// explainParseInt("3.14");
+
+// parseFloat
+
+function explainparseFloat(value) {
+  console.log("Original Value: ", value);
+  let result = parseFloat(value);
+  console.log("After parseFloat: ", result);
+}
+explainparseFloat("42");
+explainparseFloat("42px");
+explainparseFloat("3.14");
